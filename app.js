629f970460f8782b359e044d83fbb23ddbc2c397
Sam Shock <sam.shock924@gmail.com>
2022-10-03 20:42:34 -0400

    stuck on pulling values from form as number range

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
 //first I need to grab the minimum/maximum values from the input fields
-let minValue = document.getElementById("min").value;
-let maxValue = document.getElementById("max").value;
+let minValue = document.getElementById("min");
+let maxValue = document.getElementById("max");
 let numberResult = document.getElementById("generatedNumber");
 const submitButton = document.getElementById("submitButton");
+let randomNumber =
+	Math.floor(Math.random() * (maxValue.value - minValue.value + 1)) +
+	minValue.value;
+
+//I need to create a function that checks if there's a min/max number
+// function checkMinValue(minValue) {
+// 	if (minValue.value == "") {
+// 		return "empty min field";
+// 	} else {
+// 		return minValue.value;
+// 	}
+// }
+
+// function checkMaxValue(maxValue) {
+// 	if (maxValue.value == "") {
+// 		return "empty max field";
+// 	} else {
+// 		return maxValue.value;
+// 	}
+// }
 
 //next I need to generate a random number
 function generateNumber(minValue, maxValue) {
-	return (numberResult.innerText = Math.floor(Math.random() * 654));
+	// checkMinValue(minValue);
+	// checkMaxValue(maxValue);
+	return (numberResult.innerText = randomNumber);
 }
 
-generateNumber();
 //next I need to create an event listener for the submit button
+submitButton.addEventListener("click", (e) => {
+	console.log(checkMinValue(minValue));
+	console.log(checkMaxValue(maxValue));
+	e.preventDefault();
+	generateNumber();
+});
