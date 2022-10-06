9adc4e81c702dfe29579f8104ccacdc99e1f617d
Sam Shock <sam.shock924@gmail.com>
2022-10-06 00:07:46 -0400

    ugh who knows

--- app.js
+++ app.js
@@ -3,32 +3,30 @@ let minValue = document.getElementById("min");
 let maxValue = document.getElementById("max");
 let numberResult = document.getElementById("generatedNumber");
 const submitButton = document.getElementById("submitButton");
-let randomNumber =
-	Math.floor(Math.random() * (maxValue.value - minValue.value + 1)) +
-	minValue.value;
 
 //I need to create a function that checks if there's a min/max number
-// function checkMinValue(minValue) {
-// 	if (minValue.value == "") {
-// 		return "empty min field";
-// 	} else {
-// 		return minValue.value;
-// 	}
-// }
+function checkMinValue(minValue) {
+	if (minValue.value == "") {
+		return "empty min field";
+	} else {
+		return minValue.value;
+	}
+}
 
-// function checkMaxValue(maxValue) {
-// 	if (maxValue.value == "") {
-// 		return "empty max field";
-// 	} else {
-// 		return maxValue.value;
-// 	}
-// }
+function checkMaxValue(maxValue) {
+	if (maxValue.value == "") {
+		return "empty max field";
+	} else {
+		return maxValue.value;
+	}
+}
 
 //next I need to generate a random number
 function generateNumber(minValue, maxValue) {
 	// checkMinValue(minValue);
 	// checkMaxValue(maxValue);
-	return (numberResult.innerText = randomNumber);
+	// return (numberResult.innerText = Math.floor(Math.random() * 100));
+	return (numberResult.innerText = Math.floor(Math.random() * m));
 }
 
 //next I need to create an event listener for the submit button
